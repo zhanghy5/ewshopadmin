d0dda3e39930a5e6ea10f612d8a6ac2ef384ba5c
lmonkey <lmonkey@lmonkey>
2023-02-15 19:37:30 +0800

    抽取路由模块，配置默认端口

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
 
+import { resolve } from 'path';
+function pathResolve(dir:string){
+    // 获取绝对路径的方法
+    return resolve(process.cwd(), '.', dir);
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
@@ -12,5 +18,20 @@ export default defineConfig({
             resolvers: [NaiveUiResolver()]
         })
     ],
+
+    resolve:{
+        alias:[
+            {
+                // 简化路径前缀，使用绝对地址
+                find:'@',
+                replacement:(pathResolve('src'))
+            }
+        ]
+
+    },
+    server: {
+        host: 'localhost',
+        port: 8000
+    }
 })
 
